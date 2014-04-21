8c0666e11989f6a6f1f68c161efe661248aba60d
mariko honma <mr-honma@lumitec.co.jp>
2014-04-21 16:55:00 +0900

    #8 記事を編集、削除出来るようにビューにリンクとボタンを配置 コントローラひな形を作成。

--- application/controllers/articles.php
+++ application/controllers/articles.php
@@ -11,11 +11,7 @@ class Articles extends CI_Controller
 
     public function create()
     {
-        $this->load->helper('form');
-        $this->load->library('form_validation');
-
-        $this->form_validation->set_rules('title', 'Title', 'required');
-        $this->form_validation->set_rules('content', 'content', 'required');
+        $this->_setup_form_validations();
 
         if($this->form_validation->run() === FALSE)
         {
@@ -32,12 +28,33 @@ class Articles extends CI_Controller
         }
     }
 
+    function _setup_form_validations()
+    {
+        $this->load->helper('form');
+        $this->load->library('form_validation');
+
+        $this->form_validation->set_rules('title', 'Title', 'required');
+        $this->form_validation->set_rules('content', 'content', 'required');
+    }
+
     function _display_create_page($data = array())
     {
         $data['page_title'] = '新しいブログ記事を作成';
         $this->load->view('templates/header', $data);
         $this->load->view('articles/create');
         $this->load->view('templates/footer');
+    }
+
+    public function delete()
+    {
+        //$this->output->enable_profiler(TRUE);
+        $id = $this->input->post('id');
+        echo 'TODO delete : '.$id;
+    }
+
+    public function edit($id)
+    {
+        echo 'TODO edit : '.$id;
     }
 }
 /* end of controllers/articles.php */
--- application/views/articles/index.php
+++ application/views/articles/index.php
@@ -3,7 +3,13 @@
     <?php echo $this->pagination->create_links(); ?>
     <?php foreach ($articles as $item): ?>
         <div class="panel panel-default">
-            <div class="title panel-heading"><?php echo $item->title; ?></div>
+            <div class="title panel-heading">
+                <?php echo anchor("articles/edit/".$item->id, $item->title, array('title' => "記事を編集")); ?>
+                <?php echo form_open('articles/delete') ?>
+                    <?php echo form_hidden('id', $item->id) ?>
+                    <button type="submit" class="delete btn btn-danger">削除</button>
+                </form>
+            </div>
             <div class="content panel-body">
                 <?php echo $html = $this->md->defaultTransform($item->content); ?>
             </div>
