fe58c17093de8cd0d35a1aa6cd7439059a146f77
mariko honma <mr-honma@lumitec.co.jp>
2014-04-22 16:38:01 +0900

    JavaScriptライブラリの追加。ダミー画像生成。

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -8,7 +8,8 @@
     <?php echo link_tag('assets/stylesheets/style.css'); ?>
     <script src="<?php echo base_url(); ?>assets/vendor/jquery/jquery-1.11.0.min.js"></script>
     <script src="<?php echo base_url(); ?>assets/vendor/bootstrap/js/bootstrap.min.js"></script>
-    <script src="<?php echo base_url(); ?>assets/vendor/markdown/marked.js"></script>
+    <script src="<?php echo base_url(); ?>assets/vendor/marked.js"></script>
+    <script src="<?php echo base_url(); ?>assets/vendor/holder.js"></script>
     <script src="<?php echo base_url(); ?>assets/javascripts/markdown-preview.js"></script>
     <script src="<?php echo base_url(); ?>assets/javascripts/comments.js"></script>
 </head>
